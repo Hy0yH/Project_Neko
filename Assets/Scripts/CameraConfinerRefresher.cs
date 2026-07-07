59166834f9bacfcacfd21d1214a7bee3a8db4dac
Junhong <pparksj22@gmail.com>
2026-07-07 21:00:19 +0900

    Add Rooftop Map

---
+++ Assets/Scripts/CameraConfinerRefresher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraConfinerRefresher : MonoBehaviour
+{
+    private CinemachineConfiner2D confiner;
+
+    private void Awake()
+    {
+        confiner = GetComponent<CinemachineConfiner2D>();
+    }
+
+    private void Start()
+    {
+        confiner.InvalidateBoundingShapeCache();
+    }
+}
