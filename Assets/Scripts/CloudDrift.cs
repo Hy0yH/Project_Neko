3a75f70202aaf819fccf3df3d240bca8b684c591
Hy0yH <imhj1348@gmail.com>
2026-07-10 23:20:34 +0900

    Add Villa 2

--- Assets/Scripts/CloudDrift.cs
+++ Assets/Scripts/CloudDrift.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class CloudDrift : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed; // 구름 이동 속도
+    [SerializeField] private float horizontalAmount; // 구름 이동 범위
+    [SerializeField] private float horizontalSpeed; // 구름 이동 속도
     [SerializeField] private float verticalAmount; // 구름의 수직 이동 범위
     [SerializeField] private float verticalSpeed; // 구름의 수직 이동 속도
 
@@ -15,9 +16,9 @@ public class CloudDrift : MonoBehaviour
 
     private void Update()
     {
-        float x = Time.time * moveSpeed; // 시간에 따라 x 좌표 계산
+        float x = Mathf.Sin(Time.time * horizontalSpeed) * horizontalAmount; // 시간에 따라 x 좌표 계산
         float y = Mathf.Sin(Time.time * verticalSpeed) * verticalAmount; // 시간에 따라 y 좌표 계산
 
-        transform.localEulerAngles = startlocalPosition + new Vector3(x, y, 0f); // 구름의 위치 업데이트
+        transform.localPosition = startlocalPosition + new Vector3(x, y, 0f); // 구름의 위치 업데이트
     }
 }
